99e8dba599a9950805e8e1e5a95f0d88e76f9578
Delpipi <alexandrepaulkouame@gmail.com>
2025-01-18 11:14:48 +0000

    manipulate DOM and work with event

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,2 +1,29 @@
-const input = document.querySelector('#favchap')
-const input = document.querySelector('button')
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+//Events
+button.addEventListener('click', function(){
+    if(input.value.trim() !== '') {
+
+        //li element to hold each entries
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        //create detele Button
+        const deteleButton = document.createElement('button');
+        deteleButton.textContent = '❌';
+
+        li.append(deteleButton)
+        list.append(li)
+
+        deteleButton.addEventListener('click', function(){
+            list.removeChild(li);
+            input.focus();
+        });
+        
+        input.value = ''
+    }
+});
+
