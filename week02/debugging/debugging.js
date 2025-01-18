77dad2fd361467ee8e2153b107eba0ed3777a33e
Delpipi <alexandrepaulkouame@gmail.com>
2025-01-18 15:47:57 +0000

    completed javascript debugging

---
+++ week02/debugging/debugging.js
@@ -0,0 +1,12 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+area = circleArea(radius);
+radius = 4;
+area = circleArea(radius);
+
+function circleArea(radius){
+    const area = radius * PI;
+}
+
+console.log(area);
