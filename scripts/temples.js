eeb09310ded649e5dd69a09255e09af311c9dfcf
Delpipi <alexandrepaulkouame@gmail.com>
2025-01-18 23:06:18 +0000

    completed desktop responsive view

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,15 @@
-const hamButton = document.querySelector("#menu")
-const navigation = document.querySelector(".navigation")
-const links = document.querySelectorAll(".navigation a")
+//Select DOM elements
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation')
+const links = document.querySelectorAll('.navigation a')
+const currentYear = document.querySelector('#currentYear')
+const lastModified = document.querySelector('#lastModified')
+
+//use the Date Object
+const today = new Date();
+lastModified.textContent = new Date(document.lastModified);
+
+currentYear.textContent = today.getFullYear();
 
 hamButton.addEventListener('click', ()=> {
     hamButton.classList.toggle("open")
